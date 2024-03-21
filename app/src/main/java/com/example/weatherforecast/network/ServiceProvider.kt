11e9d3bb5e209bd7a1335e9db99cbbde43be49fd
Aline Souza <as.alinedossantos.as@gmail.com>
2024-03-20 21:07:12 -0300

    feature: Cria o service provider

---
+++ app/src/main/java/com/example/weatherforecast/network/ServiceProvider.kt
@@ -0,0 +1,27 @@
+package com.example.weatherforecast.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ServiceProvider {
+    private const val baseUrl = "https://api.open-meteo.com/v1/forecast/"
+
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <API> createService(apiClass: Class<API>): API = retrofit.create(apiClass)
+}
