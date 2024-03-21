f06474b79f36f7396609b8f9d99f3dea8654c060
Aline Souza <as.alinedossantos.as@gmail.com>
2024-03-21 01:48:41 -0300

    feature: Adiciona interface para requisições do app

--- app/src/main/java/com/example/weatherforecast/data/api/ServiceApi.kt
+++ app/src/main/java/com/example/weatherforecast/data/api/ServiceApi.kt
@@ -1,12 +1,10 @@
 package com.example.weatherforecast.data.api
 
-import com.example.weatherforecast.data.model.WeatherForecastResponse
-import retrofit2.http.Path
+import com.example.weatherforecast.model.WeatherForecastModel
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface ServiceApi {
-    @GET("latitude={latitude}&longitude=-38.423601&hourly=temperature_2m,relative_humidity_2m")
-    fun getWeatherForecast(
-        @Path("latitude") latitude: String
-    ): WeatherForecastResponse
+    @GET("forecast?latitude=-12.900975&longitude=-38.423601&hourly=temperature_2m,relative_humidity_2m")
+    fun getWeatherForecast(): Call<WeatherForecastModel>
 }
