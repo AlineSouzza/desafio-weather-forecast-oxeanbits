b85911e0647227a3cfb9c2262129c4dc699fc510
Aline Souza <as.alinedossantos.as@gmail.com>
2024-03-21 01:45:07 -0300

    remove

--- app/src/main/java/com/example/weatherforecast/domain/repository/WeatherForecastRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weatherforecast.domain.repository
-
-import com.example.weatherforecast.data.model.WeatherForecastResponse
-
-interface WeatherForecastRepository {
-
-    suspend fun getWeatherForecast(weatherForecast: String): WeatherForecastResponse
-}
