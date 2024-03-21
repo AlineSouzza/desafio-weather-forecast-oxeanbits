c048fdf3668b9fc4215dab438d8867dd69f21d5a
Aline Souza <as.alinedossantos.as@gmail.com>
2024-03-20 21:05:47 -0300

    feature: Cria o service api para acesso de dados retornados da api

---
+++ app/src/main/java/com/example/weatherforecast/data/api/ServiceApi.kt
@@ -0,0 +1,13 @@
+package com.example.weatherforecast.data.api
+
+import com.example.weatherforecast.data.model.WeatherForecastResponse
+import retrofit2.http.Path
+import retrofit2.http.GET
+
+
+interface ServiceApi {
+    @GET("latitude={latitude}&longitude=-38.423601&hourly=temperature_2m,relative_humidity_2m")
+    suspend fun getWeatherForecast(
+        @Path("latitude") latitude: String
+    ): WeatherForecastResponse
+}
