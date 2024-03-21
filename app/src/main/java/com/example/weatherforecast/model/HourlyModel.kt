295e5e2e6dc8970de7de2670baca52f0d6753832
Aline Souza <as.alinedossantos.as@gmail.com>
2024-03-21 01:46:39 -0300

    feature: Adiciona modelos da resposta da api

---
+++ app/src/main/java/com/example/weatherforecast/model/HourlyModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherforecast.model
+
+data class HourlyModel(
+    val temperature_2m: List<Double>?,
+    val relative_humidity_2m: List<Double>?,
+    val time: List<String>?,
+)
---
+++ app/src/main/java/com/example/weatherforecast/model/WeatherForecastModel.kt
@@ -0,0 +1,16 @@
+package com.example.weatherforecast.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+data class WeatherForecastModel(
+    val latitude: Double?,
+    val longitude: Double?,
+    val timezone: String?,
+    val timezone_abbreviation: String?,
+    val elevation: Double?,
+    val hourly_units: HourlyUnitsModel?,
+    val hourly: HourlyModel?,
+)
+
+
