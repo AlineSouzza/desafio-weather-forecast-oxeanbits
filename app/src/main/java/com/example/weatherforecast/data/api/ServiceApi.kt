252c8f78d765fc0e5d06a34a1323154e6aeb3563
Aline Souza <as.alinedossantos.as@gmail.com>
2024-03-22 19:25:08 -0300

    feature: Adiciona novos parâmetros na rota

--- app/src/main/java/com/example/weatherforecast/data/api/ServiceApi.kt
+++ app/src/main/java/com/example/weatherforecast/data/api/ServiceApi.kt
@@ -3,8 +3,12 @@ package com.example.weatherforecast.data.api
 import com.example.weatherforecast.model.WeatherForecastModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ServiceApi {
-    @GET("forecast?latitude=-12.900975&longitude=-38.423601&hourly=temperature_2m,relative_humidity_2m")
-    fun getWeatherForecast(): Call<WeatherForecastModel>
+    @GET("forecast?hourly=temperature_2m&daily=uv_index_max&current=relative_humidity_2m&daily=precipitation_probability_max&current=is_day")
+    fun getWeatherForecast(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double
+    ): Call<WeatherForecastModel>
 }
