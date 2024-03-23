6d33edae86a001ac0861e69ae7f643dedd28b305
Aline Souza <as.alinedossantos.as@gmail.com>
2024-03-22 22:03:17 -0300

    feature: Adiciona variáveis que mapeam o sunrise e sunset

--- app/src/main/java/com/example/weatherforecast/model/DailyModel.kt
+++ app/src/main/java/com/example/weatherforecast/model/DailyModel.kt
@@ -4,4 +4,6 @@ data class DailyModel(
     val uv_index_max: List<Double>?,
     val time: List<String>?,
     val precipitation_probability_max: List<Int>?,
+    val sunrise: List<String>,
+    val sunset: List<String>,
 )
